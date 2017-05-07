1803c2a35264d410b5528d7a6ab76aac461cd57e
Rene Avontuur <rene.avontuur@chello.nl>
2017-05-07 11:54:26 +0200

    added showing display data (wip)

--- hebbian-waterfall-system-dynamics/ex3.m
+++ hebbian-waterfall-system-dynamics/ex3.m
@@ -9,6 +9,15 @@ fprintf('Loading Data ...\n')
 load('ex3data1.mat'); % training data stored in arrays X, y
 m = size(X, 1);
 
+% Randomly select 100 data points to display
+rand_indices = randperm(m);
+sel = X(rand_indices(1:100), :);
+
+displayData(sel);
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
 % total duration
 deltaT = 0.02;
 
